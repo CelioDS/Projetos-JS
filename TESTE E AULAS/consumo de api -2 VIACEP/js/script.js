bd71eb7c6777478d1fa866e59c71ac15d37e5ba1
Célio Da Silva <celio01t@gmail.com>
2023-07-20 12:31:12 -0300

    style : nav header

--- TESTE E AULAS/consumo de api -2 VIACEP/js/script.js
+++ TESTE E AULAS/consumo de api -2 VIACEP/js/script.js
@@ -1,41 +1,51 @@
-const url = 'https://viacep.com.br/ws/01001000/json/'
+const url = "https://viacep.com.br/ws/01001000/json/";
 
-let h1 = document.querySelector('h1')
-h1.innerHTML = 'teste'
+let h1 = document.querySelector("h1");
+h1.innerHTML = "teste";
 var elemento_pai = document.body;
-var teste = document.createElement('h2')
+var teste = document.createElement("h2");
 elemento_pai.appendChild(teste);
-
 
 //get all post
 async function getAllPost() {
-    // primeira resposta da api
-    const response = await fetch(url)
+  // primeira resposta da api
+  const response = await fetch(url);
 
-    const status = response.status
+  const status = response.status;
 
-    if (status === 200) {
-        const data = await response.json()
-        console.log(data)
-    } else {
-        console.log(response)
-    }
+  if (status === 200) {
+    const data = await response.json();
+    console.log(data);
+  } else {
+    console.log(response);
+  }
 }
-getAllPost(url)
-
-
+getAllPost(url);
 
 fetch(`https://viacep.com.br/ws/01001000/json/`)
-    .then(response => {
-        return response.json()
-    })
-    .then(data => {
-        Object.keys(data).forEach((key) => {
-            var teste = document.createElement('li')
-            elemento_pai.appendChild(teste);
-            teste.innerHTML = key + ' ' + data[key]
-        })
-    })
-    .catch(error => {
-        console.error('Ocorreu um erro ao consumir a API: ', error);
-    })    
+  .then((response) => {
+    return response.json();
+  })
+  .then((data) => {
+    Object.keys(data).forEach((key) => {
+      var teste = document.createElement("li");
+      elemento_pai.appendChild(teste);
+      teste.innerHTML = key + " " + data[key];
+    });
+  })
+  .catch((error) => {
+    console.error("Ocorreu um erro ao consumir a API: ", error);
+  });
+
+fetch(`https://businesshere.onrender.com`)
+  .then((response) => {
+
+
+    return response.json();
+  })
+  .then((data) => {
+    console.log(data);
+  })
+  .catch((error) => {
+    console.error("Ocorreu um erro ao consumir a APIa: ", error);
+  });
---
+++ TESTE E AULAS/pop up/js/script.js
@@ -0,0 +1,13 @@
+const popup = document.getElementById("popup") // pegando a div 
+
+
+setTimeout( () =>{
+    popup.style.display = 'flex'
+}, 1000)
+
+setTimeout( () =>{
+    popup.style.display = 'none'
+}, 5000)
+
+
+
